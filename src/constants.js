6cbcdd752f43ac26ab68d79592394c3ba444c645
Devlyn Dorfer <devlyndorfer@gmail.com>
2021-10-25 02:30:34 +0000

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xF2F7aF84730D54EBf3bee12b18A5a410ee55bA1c';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
